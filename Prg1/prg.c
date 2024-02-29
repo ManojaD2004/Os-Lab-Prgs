ca310e526bd52d80d8cff8f801b0bf6ca7acc9ab
ManojaD2004 <manojadkc2004@gmail.com>
2024-02-29 09:15:12 +0530

    Save OS Prgs

---
+++ Prg1/prg.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void main()
+{
+    pid_t pid = 0;
+    int status;
+    pid = fork();
+    if (pid == 0)
+    {
+        printf("I am the child!");
+        execl("/bin/ls", "ls", "-l", "/home/manojad", (char *)0);
+        perror("In exec(): ");
+    }
+    if (pid > 0)
+    {
+        printf("I am the parent %d, and the child is %d.\n", getppid(), pid);
+        pid = wait(&status);
+        printf("End of process %d: ", pid);
+        if (WIFEXITED(status))
+        {
+            printf("The process ended with exit(%d).\n", WEXITSTATUS(status));
+        }
+        if (WIFSIGNALED(status))
+        {
+            printf("The process ended with kill -%d.\n", WTERMSIG(status));
+        }
+    }
+    if (pid < 0)
+    {
+        perror("In fork():");
+    }
+    exit(0);
+}
